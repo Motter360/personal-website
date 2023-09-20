6814bdcab675f146677efcd0335ca10f685e8cec
Thomas <thomas@Thomass-MacBook-Pro.local>
2023-09-19 19:01:49 -0500

    sytax and demo changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Hello World!</h1> 
+     <h1>Hello Open SGF!</h1> 
+     <h2>Please give me a job</h2>
+     <h2>Call Levi Zitting</h2>
+     <h2>417-869-4926</h2>
     </div>
   );
 }
