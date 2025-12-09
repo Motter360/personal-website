4cd441186179018d5bb20e51d0acdff743312f3e
Thomas Hammon <hammonthomas@gmail.com>
2025-12-09 16:14:08 -0600

    added card component and portfolio section

--- src/sections/components/card.js
+++ src/sections/components/card.js
@@ -1,18 +1,18 @@
 import React from "react";
 
 export default function Card(props){
-    const {src, alt, title, description, stack} = props;
+    const {src, alt, title, description, stack, link} = props;
     return(
         <div className="card">
             <div className="portfolioPhotoSection">
-                <div className="spacer">
-                    <img className="portfolioPhoto" src={src} alt={alt} width="90%" height="90%"/>
-                </div>
+                <a href={link}>
+                    <img src={src} alt={alt} width="100%" height="100%"/>
+                </a>
             </div>
             <div className="portfolioDetails">
                 <h4>{title}</h4>
                 <p>{description}</p>
-                <ul>
+                <ul className="flex wrap">
                     {stack.map(item =>(<li>{item}</li>))}
                 </ul>
             </div>
--- src/sections/components/portfolio.js
+++ src/sections/components/portfolio.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import Card from './card.js'
+import data from '../../data/portfolioData.js';
 
 export function Portfolio(){
 
-    const testItem = {
-        src: "https://images.unsplash.com/photo-1491466424936-e304919aada7?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        alt:"Cool Photo",
-        title:"Great Title",
-        description: "Indepth description",
-        stack: ["React", "Git", "JS"]
-    }
     return(
         <div id="portfolio" className='section dm-shade-1'>
             <p className='section-title'>Portfolio</p>
-                <div className='flex buffer'>
-                    <Card 
-                        src={testItem.src}
-                        alt={testItem.alt}
-                        title={testItem.title}
-                        description={testItem.description}
-                        stack={testItem.stack}
-                    />
+                <div className='flex column center buffer'>
+                    {data.map(item => <Card 
+                            src={item.src}
+                            alt={item.alt}
+                            title={item.title}
+                            description={item.description}
+                            stack={item.stack}
+                            link={item.link}
+                        />)
+                    }
                 </div>
         </div>
     )
